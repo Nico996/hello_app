ed6c5f7c0316be33f13c3601d2a0ed701ee54911
Nico996 <naf.1996@outlook.com.ar>
2018-10-07 13:01:08 -0300

    update

--- shoppingCartService/src/main/java/com/globant/cartService/ShoppingCartServiceApplication.java
+++ shoppingCartService/src/main/java/com/globant/cartService/ShoppingCartServiceApplication.java
@@ -2,11 +2,23 @@ package com.globant.cartService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ 
 
 @SpringBootApplication
-public class ShoppingCartServiceApplication {
+@Import(com.globant.cartService.documentation.SwaggerConfiguration.class)
+public class ShoppingCartServiceApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingCartServiceApplication.class, args);
+	}
+	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+ 
+           registry.addResourceHandler("swagger-ui.html")
+                    .addResourceLocations("classpath:/META-INF/resources/");
 	}
 }
